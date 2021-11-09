887bcaa279c5352b8f93be0719046ae34bd38309
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-09 16:16:24 +0000

    DPP-5 #comment dependencies added to pom.xml file
    Rested App Spring Boot Project created.

---
+++ RestedPracticalProject/src/main/java/com/qa/rested/StarterProject1Application.java
@@ -0,0 +1,13 @@
+package com.qa.rested;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterProject1Application.class, args);
+	}
+
+}
---
+++ RestedPracticalProject/src/test/java/com/qa/rested/StarterProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.rested;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

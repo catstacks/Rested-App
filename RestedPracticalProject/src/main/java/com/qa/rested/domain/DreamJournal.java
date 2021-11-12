b72f48878ebe5da5327278416859721ddac63be7
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-12 03:08:33 +0000

    Create Dream Journal

--- RestedPracticalProject/src/main/java/com/qa/rested/domain/DreamJournal.java
+++ RestedPracticalProject/src/main/java/com/qa/rested/domain/DreamJournal.java
@@ -1,5 +1,65 @@
 package com.qa.rested.domain;
 
-public class DreamJournal {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Optional;
+import java.util.UUID;
+
+@Entity
+@Table(name = "journal")
+public final class DreamJournal {
+
+    @Id
+    @Column(name = "id", columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column(name = "sleep_rating", length = 2, nullable = false)
+    private Integer sleepRating;
+
+    @Column(name = "description", length = 500, nullable = true)
+    private String description;
+
+    public DreamJournal(
+            final UUID id,
+            final Integer sleepRating,           
+            final String description
+    ) {
+        this.id = Optional.ofNullable(id).orElse(UUID.randomUUID());
+        this.sleepRating = sleepRating; 
+        this.description = description;
+    }
+
+    public DreamJournal() {
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(final UUID id) {
+        this.id = id;
+    }
+
+
+    public Integer getSleepRating() {
+        return sleepRating;
+    }
+
+    public void setSleepRating(final Integer sleepRating) {
+        this.sleepRating = sleepRating;
+    }
+
+
+    public String getDescription() {
+        return description.trim();
+    }
+
+    public void setDescription(final String description) {
+        this.description = (description == null) ? "" : description.trim();
+    }
 
 }
+

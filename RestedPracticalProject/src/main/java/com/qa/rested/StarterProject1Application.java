08a75128afb75ca4f88dd2944761e12a84bb1ae1
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-12 01:31:01 +0000

    Update RestedUser

--- RestedPracticalProject/src/main/java/com/qa/rested/StarterProject1Application.java
+++ RestedPracticalProject/src/main/java/com/qa/rested/StarterProject1Application.java
@@ -1,13 +1,16 @@
 package com.qa.rested;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StarterProject1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StarterProject1Application.class, args);
+	public static void main(String[] args) throws Exception {
+		ApplicationContext AC = SpringApplication.run(StarterProject1Application.class, args);
+		
 	}
 
 }
--- RestedPracticalProject/src/main/java/com/qa/rested/domain/RestedUser.java
+++ RestedPracticalProject/src/main/java/com/qa/rested/domain/RestedUser.java
@@ -18,9 +18,8 @@ public class RestedUser {
 	private String email;
 	
 	
-	public RestedUser(Long id, String firstName, String lastName, Integer age, String email) {
+	public RestedUser(String firstName, String lastName, Integer age, String email) {
 		super();
-		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.age = age;

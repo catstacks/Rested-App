68194fbe7d588502c62708bd90b1028cc1bc1083
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-12 04:03:22 +0000

    Create DTO and abstract

---
+++ RestedPracticalProject/src/main/java/com/qa/rested/dto/RestedDTO.java
@@ -0,0 +1,160 @@
+package com.qa.rested.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.qa.rested.domain.RestedUser;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public final class RestedDTO implements Serializable {
+
+    @JsonIgnore private UUID id;
+	private Date dob;
+	private double age;
+	private Integer sleepQuality;
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String timeZone;
+
+    public RestedDTO(
+    		final UUID id,
+    		final Date dob,
+    		final double age,
+    		final Integer sleepQuality,
+    		final String email,
+    		final String password,
+    		final String firstName,
+    		final String lastName,
+    		final String timeZone
+    ) 
+    
+    {	this.id = id;
+    	this.dob = (Date) dob.clone();
+    	this.age =age;
+    	this.sleepQuality = sleepQuality;
+    	this.email = email;
+    	this.password = password;
+    	this.firstName = firstName;
+    	this.lastName = lastName;
+    	this.timeZone = timeZone;
+    }
+
+    public RestedDTO() {
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(final UUID id) {
+        this.id = id;
+    }
+
+
+    public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public double getAge() {
+		return age;
+	}
+
+	public void setAge(double age) {
+		this.age = age;
+	}
+
+	public Integer getSleepQuality() {
+		return sleepQuality;
+	}
+
+	public void setSleepQuality(Integer sleepQuality) {
+		this.sleepQuality = sleepQuality;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(final String timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        boolean equals = false;
+        if (other instanceof RestedDTO) {
+            final RestedDTO that = (RestedDTO) other;
+            equals = this.getId().equals(that.getId())                 
+                    && this.getDob().toString().equals(that.getDob().toString())
+                    && this.getAge() == that.getAge()
+                    && this.getSleepQuality() == that.getSleepQuality()
+                    && this.getEmail().equals(that.getEmail())
+                    && this.getPassword().equals(that.getPassword())
+                    && this.getFirstName().equals(that.getFirstName())
+                    && this.getLastName().equals(that.getLastName())
+                    && this.getTimeZone().equals(that.getTimeZone());   
+        }
+        return equals;
+    }
+
+    @Override
+    public int hashCode() {
+        final int idHash = (id == null) ? 0 : id.hashCode();
+        final int dobHash = (dob == null) ? 0 : dob.hashCode();
+        final int ageHash = Double.valueOf(age).hashCode();
+        final int sleepQualityHash = (sleepQuality == null) ? 0 : sleepQuality.hashCode();
+        final int emailHash = (email == null) ? 0 : email.hashCode();
+        final int firstNameHash = (firstName == null) ? 0 : firstName.hashCode();
+        final int lastNameHash = (lastName == null) ? 0 : lastName.hashCode();
+
+
+        return idHash + dobHash + ageHash + sleepQualityHash + emailHash
+                + firstNameHash + lastNameHash;
+    }
+
+}
--- RestedPracticalProject/src/main/java/com/qa/rested/service/RestedService.java
+++ RestedPracticalProject/src/main/java/com/qa/rested/service/RestedService.java
@@ -1,5 +1,12 @@
 package com.qa.rested.service;
 
+import com.qa.rested.dto.RestedDTO;
+
 public class RestedService {
+
+	public RestedDTO findByEmail(String attribute) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }

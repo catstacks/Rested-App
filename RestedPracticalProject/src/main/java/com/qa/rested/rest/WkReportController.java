38cb261f68ce78ec0804f9b2d842008e069401b8
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-14 06:35:22 +0000

    Update user-schema

--- RestedPracticalProject/src/main/java/com/qa/rested/rest/WkReportController.java
+++ RestedPracticalProject/src/main/java/com/qa/rested/rest/WkReportController.java
@@ -7,13 +7,17 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.rested.domain.RestedUser;
 import com.qa.rested.domain.WeeklyReportData;
 import com.qa.rested.dto.RestedDTO;
 import com.qa.rested.service.WkReportService;
 
+@RestController
+@RequestMapping("/rested")
 public class WkReportController {
 	
 	 private final WkReportService wkReportService;
--- RestedPracticalProject/src/main/resources/user-schema.sql
+++ RestedPracticalProject/src/main/resources/user-schema.sql
@@ -5,9 +5,30 @@ CREATE TABLE `rested_users` (
 	`dob` date NOT NULL,
 	`age` integer NOT NULL,
 	`sleep_quality` integer NOT NULL,
-	`email` varchar(255) NOT NULL,
-	`password` varchar(255),
-	`first_name` varchar(255) NOT NULL, 
-	`last_name` varchar(255) NOT NULL,	
+	`email` varchar(100) NOT NULL,
+	`password` varchar(100),
+	`first_name` varchar(20) NOT NULL, 
+	`last_name` varchar(20) NOT NULL,	
+	PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `daily_sleep` CASCADE; 
+
+CREATE TABLE `daily_sleep` (
+	`id` binary(16) NOT NULL, 
+	`user_id` COLLATE utf8_bin NOT NULL,
+	`journal_id` COLLATE utf8_bin NOT NULL,
+	`date` date NOT NULL,
+	`hours` integer NOT NULL,
+	`minutes` integer NOT NULL,	
+	PRIMARY KEY (`id`)
+); ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `dream_journal` CASCADE; 
+
+CREATE TABLE `daily_sleep` (
+	`id` binary(16) NOT NULL, 
+	`sleep_rating` integer(2) NOT NULL,
+	`description` varchar(500),	
 	PRIMARY KEY (`id`)
 );

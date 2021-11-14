7a997d9b1bb91516d63bb594483ddc298f1e0d1a
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-14 18:49:25 +0000

    Update user-schema

--- RestedPracticalProject/src/main/resources/user-schema.sql
+++ RestedPracticalProject/src/main/resources/user-schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `RESTED_USER` CASCADE; 
 
 CREATE TABLE `RESTED_USER` (
-	`id` binary(16) NOT NULL, 
+	`id` integer NOT NULL, 
 	`dob` date NOT NULL,
 	`age` integer NOT NULL,
 	`sleep_quality` integer NOT NULL,
@@ -15,19 +15,19 @@ CREATE TABLE `RESTED_USER` (
 DROP TABLE IF EXISTS `daily_sleep` CASCADE; 
 
 CREATE TABLE `daily_sleep` (
-	`id` binary(16) NOT NULL, 
-	`user_id` COLLATE utf8_bin NOT NULL,
-	`journal_id` COLLATE utf8_bin NOT NULL,
+	`id` integer NOT NULL, 
+	`user_id` integer NOT NULL,
+	`journal_id` integer NOT NULL,
 	`date` date NOT NULL,
 	`hours` integer NOT NULL,
 	`minutes` integer NOT NULL,	
 	PRIMARY KEY (`id`)
-); ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+); 
 
 DROP TABLE IF EXISTS `dream_journal` CASCADE; 
 
 CREATE TABLE `dream_journal` (
-	`id` binary(16) NOT NULL, 
+	`id` integer NOT NULL, 
 	`sleep_rating` integer(2) NOT NULL,
 	`description` varchar(500),	
 	PRIMARY KEY (`id`)
@@ -36,45 +36,45 @@ CREATE TABLE `dream_journal` (
 DROP TABLE IF EXISTS `screen_time` CASCADE; 
 
 CREATE TABLE `screen_time` (
-	`id` binary(16) NOT NULL, 
-	`user_id` COLLATE utf8_bin NOT NULL,
+	`id` integer NOT NULL, 
+	`user_id` integer NOT NULL,
 	`date` date NOT NULL,
 	`hours` integer NOT NULL,
 	`minutes` integer NOT NULL,	
 	PRIMARY KEY (`id`)
-); ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+);
 
 DROP TABLE IF EXISTS `water` CASCADE; 
 
 CREATE TABLE `water` (
-	`id` binary(16) NOT NULL, 
-	`user_id` COLLATE utf8_bin NOT NULL,
+	`id` integer NOT NULL, 
+	`user_id` integer NOT NULL,
 	`name` varchar(50) NOT NULL,
 	`default_serving` varchar(10) NOT NULL,
 	`serving_size_amount` double NOT NULL,
 	PRIMARY KEY (`id`)
-); ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+);
 
 DROP TABLE IF EXISTS `water_consumed` CASCADE; 
 
 CREATE TABLE `water_consumed` (
-	`id` binary(16) NOT NULL, 
-	`user_id` COLLATE utf8_bin NOT NULL,
-	`water_id` COLLATE utf8_bin NOT NULL,
+	`id` integer NOT NULL, 
+	`user_id` integer NOT NULL,
+	`water_id` integer NOT NULL,
 	`date` date NOT NULL,
 	`serving_size` varchar(10) NOT NULL,
 	`serving_amount` double NOT NULL,
 	PRIMARY KEY (`id`)
-); ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+); 
 
 DROP TABLE IF EXISTS `weekly_report_data` CASCADE; 
 
 CREATE TABLE `weekly_report_data` (
-	`id` binary(16) NOT NULL, 
-	`user_id` COLLATE utf8_bin NOT NULL,
+	`id` integer NOT NULL, 
+	`user_id` integer NOT NULL,
 	`date` date NOT NULL,
 	`weekly_sleep` double NOT NULL,
 	`weekly_water` double NOT NULL,
 	`weekly_screen_time` double NOT NULL,
 	PRIMARY KEY (`id`)
-); ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+);

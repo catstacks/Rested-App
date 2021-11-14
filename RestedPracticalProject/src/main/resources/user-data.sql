4ae8840a13b651d3be67614c3affd8693480fca3
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-14 15:55:44 +0000

    update user data

--- RestedPracticalProject/src/main/resources/user-data.sql
+++ RestedPracticalProject/src/main/resources/user-data.sql
@@ -4,3 +4,45 @@ INSERT INTO `RESTED_USER`
  
 	VALUES 
 (01/01/2001, 20, 1, 'Test.User@domain.com', 'password', 'Billy', 'Bob');
+
+INSERT INTO `daily_sleep` 
+(`date`, `hours`, `minutes`)
+
+ 
+	VALUES 
+(11/11/2021, 6, 5);
+
+INSERT INTO `RESTED_USER` 
+(`dob`, `age`, `sleep_quality`, `email`, `password`, `first_name`, `last_name`)
+
+ 
+	VALUES 
+(01/01/2001, 20, 1, 'Test.User@domain.com', 'password', 'Billy', 'Bob');
+
+INSERT INTO `RESTED_USER` 
+(`dob`, `age`, `sleep_quality`, `email`, `password`, `first_name`, `last_name`)
+
+ 
+	VALUES 
+(01/01/2001, 20, 1, 'Test.User@domain.com', 'password', 'Billy', 'Bob');
+
+INSERT INTO `RESTED_USER` 
+(`dob`, `age`, `sleep_quality`, `email`, `password`, `first_name`, `last_name`)
+
+ 
+	VALUES 
+(01/01/2001, 20, 1, 'Test.User@domain.com', 'password', 'Billy', 'Bob');
+
+INSERT INTO `RESTED_USER` 
+(`dob`, `age`, `sleep_quality`, `email`, `password`, `first_name`, `last_name`)
+
+ 
+	VALUES 
+(01/01/2001, 20, 1, 'Test.User@domain.com', 'password', 'Billy', 'Bob');
+
+INSERT INTO `RESTED_USER` 
+(`dob`, `age`, `sleep_quality`, `email`, `password`, `first_name`, `last_name`)
+
+ 
+	VALUES 
+(01/01/2001, 20, 1, 'Test.User@domain.com', 'password', 'Billy', 'Bob');

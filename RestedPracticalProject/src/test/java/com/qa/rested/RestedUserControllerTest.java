2bdb73fcaabcd7df994429facbc676f9389b7903
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-14 19:58:46 +0000

    Update RestedControllerTest with test templates

--- RestedPracticalProject/src/test/java/com/qa/rested/RestedUserControllerTest.java
+++ RestedPracticalProject/src/test/java/com/qa/rested/RestedUserControllerTest.java
@@ -1,5 +1,144 @@
 package com.qa.rested;
 
+import org.junit.jupiter.api.Test;
+
 public class RestedUserControllerTest {
+	
+	@Test
+	public void shouldCreateNewUser() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldCreateNewSleep() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldCreateNewDream() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldCreateNewScreen() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldCreateNewWater() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldCreateNewWaterConsumed() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldCreateNewReport() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldGetUser() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldGetSleep() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldGetDream() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldGetScreen() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldGetWater() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldGetWaterConsumed() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldGetReport() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldReplaceSleep() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldReplaceDream() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldReplaceScreen() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldReplaceWater() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldReplaceWaterConsumed() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldReplaceReport() {
+	    fail("Fail");
+	}
+	
+	//
+	
+	@Test
+	public void shouldDeleteUser() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldDeleteSleep() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldDeleteDream() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldDeleteScreen() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldDeleteWater() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldDeleteWaterConsumed() {
+	    fail("Fail");
+	}
+	
+	@Test
+	public void shouldDeleteReport() {
+	    fail("Fail");
+	}
 
 }

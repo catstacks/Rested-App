7f49fe890d58e2713657e26e220285fcaf59f1fa
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-14 07:02:01 +0000

    Update user-schema

--- RestedPracticalProject/src/main/resources/user-schema.sql
+++ RestedPracticalProject/src/main/resources/user-schema.sql
@@ -32,3 +32,49 @@ CREATE TABLE `dream_journal` (
 	`description` varchar(500),	
 	PRIMARY KEY (`id`)
 );
+
+DROP TABLE IF EXISTS `screen_time` CASCADE; 
+
+CREATE TABLE `screen_time` (
+	`id` binary(16) NOT NULL, 
+	`user_id` COLLATE utf8_bin NOT NULL,
+	`date` date NOT NULL,
+	`hours` integer NOT NULL,
+	`minutes` integer NOT NULL,	
+	PRIMARY KEY (`id`)
+); ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `water` CASCADE; 
+
+CREATE TABLE `water` (
+	`id` binary(16) NOT NULL, 
+	`user_id` COLLATE utf8_bin NOT NULL,
+	`name` varchar(50) NOT NULL,
+	`default_serving` varchar(10) NOT NULL,
+	`serving_size_amount` double NOT NULL,
+	PRIMARY KEY (`id`)
+); ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `water_consumed` CASCADE; 
+
+CREATE TABLE `water_consumed` (
+	`id` binary(16) NOT NULL, 
+	`user_id` COLLATE utf8_bin NOT NULL,
+	`water_id` COLLATE utf8_bin NOT NULL,
+	`date` date NOT NULL,
+	`serving_size` varchar(10) NOT NULL,
+	`serving_amount` double NOT NULL,
+	PRIMARY KEY (`id`)
+); ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `weekly_report_data` CASCADE; 
+
+CREATE TABLE `weekly_report_data` (
+	`id` binary(16) NOT NULL, 
+	`user_id` COLLATE utf8_bin NOT NULL,
+	`date` date NOT NULL,
+	`weekly_sleep` double NOT NULL,
+	`weekly_water` double NOT NULL,
+	`weekly_screen_time` double NOT NULL,
+	PRIMARY KEY (`id`)
+); ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
